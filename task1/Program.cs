f53ab61a37820e1e1a91bb2d658ace40745bfc2d
Ismoil <zajnalovismoil07@gmail.com>
2025-12-24 16:29:45 +0300

    finish project

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,16 +2,19 @@
 
 Person[] people = new Person[3];
 Console.Write("Enter name of student 1: ");
-people[0] = new Student(Console.ReadLine()!);
+Student student = new Student(Console.ReadLine()!);
+people[0] = student;
 Console.Write("Enter name of student 2: ");
-people[1] = new Student(Console.ReadLine()!);
+Student student2 = new Student(Console.ReadLine()!);
+people[1] = student2;
 Console.Write("Enter name of teacher: ");
-people[2] = new Student(Console.ReadLine()!);
+
+Teacher teacher = new Teacher(Console.ReadLine()!);
+people[2] = teacher;
 foreach (var item in people)
 {
     System.Console.WriteLine(item.ToString());
 }
-Student student = new Student("Alisher");
 student.Study();
-Teacher teacher = new Teacher("Cuhrob");
+student2.Study();
 teacher.Explain();
